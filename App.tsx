7c56d2dc4d17df71a64f160639d8f4bfc446779d
BernardoModerno <bernardo.moderno@gmail.com>
2022-10-30 16:38:49 -0300

    mudança

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 export default function App() {
   return (
@@ -9,6 +13,7 @@ export default function App() {
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
