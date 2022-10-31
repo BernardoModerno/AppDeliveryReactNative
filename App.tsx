9e42cfa71251831b0a0369af3e006fb843e1a813
BernardoModerno <bernardo.moderno@gmail.com>
2022-10-31 17:16:55 -0300

    Tela Principal (Parte 2)

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import {
   Image,
   StyleSheet,
+  Text,
   View,
 } from 'react-native';
 
@@ -11,6 +12,13 @@ export default function App() {
         source={ require('./assets/chef.jpg') } 
         style={ styles.imageBackground }
         />
+      <View style={styles.logoContainer}>
+        <Image
+          source={ require('./assets/logo.png') }
+          style={ styles.logoImage }
+        />
+        <Text style={ styles.logoText } >FOOD APP</Text>
+      </View>
       <View style={ styles.form }>
 
       </View>
@@ -38,5 +46,21 @@ const styles = StyleSheet.create({
     bottom: 0,
     borderTopRightRadius: 40,
     borderTopLeftRadius: 40
+  },
+  logoContainer: {
+    position: 'absolute',
+    alignSelf: 'center',
+    top: '15%'
+  },
+  logoImage: {
+    width: 100,
+    height: 100
+  },
+  logoText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 10,
+    fontWeight: 'bold'
   }
 });
