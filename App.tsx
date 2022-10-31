80e97eabaa53ffe9de1d767f174081c3b8acadf5
BernardoModerno <bernardo.moderno@gmail.com>
2022-10-30 21:24:31 -0300

    Tela Principal (Parte 1)

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import {
+  Image,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={ require('./assets/chef.jpg') } 
+        style={ styles.imageBackground }
+        />
+      <View style={ styles.form }>
+
+      </View>
     </View>
   );
 }
@@ -18,8 +22,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
   },
+  imageBackground: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.7,
+    bottom: '30%'
+  }, 
+  form: {
+    width: '100%',
+    height: '40%',
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40
+  }
 });
