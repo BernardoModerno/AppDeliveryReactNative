a28a5937b706b1948649b177f0d12fe1ccef082c
BernardoModerno <bernardo.moderno@gmail.com>
2022-10-31 22:58:07 -0300

    Tela Principal (Parte 3)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import {
+  Button,
   Image,
   StyleSheet,
   Text,
+  TextInput,
+  ToastAndroid,
   View,
 } from 'react-native';
 
@@ -20,6 +23,44 @@ export default function App() {
         <Text style={ styles.logoText } >FOOD APP</Text>
       </View>
       <View style={ styles.form }>
+        <Text style={styles.formText} >Entrar:</Text>
+        <View style={styles.formInput} >
+           <Image
+             style={ styles.formIcon }
+             source={ require('./assets/email.png') }
+           />
+           <TextInput
+             style={ styles.formTextInput }
+             placeholder='Email'
+             keyboardType='email-address'
+           />
+        </View>
+
+        <View style={styles.formInput} >
+           <Image
+             style={ styles.formIcon }
+             source={ require('./assets/password.png') }
+           />
+           <TextInput
+             style={ styles.formTextInput }
+             placeholder='Senha'
+             keyboardType='default'
+             secureTextEntry={true}
+           />
+        </View>
+
+        <View style={{ marginTop: 30 }}>
+           <Button 
+             title='ENTRAR'
+             onPress={ () => ToastAndroid.show( 'CLICK', ToastAndroid.LONG ) }
+             color='orange'
+           />
+        </View>
+
+        <View style={ styles.formRegister }>
+          <Text>Não tem conta?</Text>
+          <Text style={ styles.formRegisterText }>Registrar-se</Text>
+        </View>
 
       </View>
     </View>
@@ -45,7 +86,40 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     borderTopRightRadius: 40,
-    borderTopLeftRadius: 40
+    borderTopLeftRadius: 40,
+    padding: 30
+  },
+  formText: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  formIcon: {
+    width: 25,
+    height: 25,
+    marginTop: 5
+  },
+  formInput: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  formTextInput: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#AAAAAA',
+    marginLeft: 15
+  },
+  formRegister: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  formRegisterText: {
+    fontStyle: 'italic',
+    color: 'orange',
+    borderBottomWidth: 1,
+    borderBottomColor: 'orange',
+    fontWeight: 'bold',
+    marginLeft: 10
   },
   logoContainer: {
     position: 'absolute',
@@ -62,5 +136,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 10,
     fontWeight: 'bold'
-  }
+  },
+  
 });
