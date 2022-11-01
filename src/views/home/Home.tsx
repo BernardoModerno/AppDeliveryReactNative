c95188c22801d58186cd55247a875bd223a0b45e
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-01 10:32:08 -0300

    Navigation Stack (HomeScreen)

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,140 @@
+import React from 'react';
+
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  ToastAndroid,
+  View,
+} from 'react-native';
+
+import { RoundedButton } from '../../components/RoundedButton';
+
+export const HomeScreen = () => {
+    return (
+    <View style={styles.container}>
+        <Image
+        source={ require('../../../assets/chef.jpg') } 
+        style={ styles.imageBackground }
+        />
+        <View style={styles.logoContainer}>
+        <Image
+            source={ require('../../../assets/logo.png') }
+            style={ styles.logoImage }
+        />
+        <Text style={ styles.logoText } >FOOD APP</Text>
+        </View>
+        <View style={ styles.form }>
+        <Text style={styles.formText} >Entrar:</Text>
+        <View style={styles.formInput} >
+            <Image
+                style={ styles.formIcon }
+                source={ require('../../../assets/email.png') }
+            />
+            <TextInput
+                style={ styles.formTextInput }
+                placeholder='Email'
+                keyboardType='email-address'
+            />
+        </View>
+
+        <View style={styles.formInput} >
+            <Image
+                style={ styles.formIcon }
+                source={ require('../../../assets/password.png') }
+            />
+            <TextInput
+                style={ styles.formTextInput }
+                placeholder='Senha'
+                keyboardType='default'
+                secureTextEntry={true}
+            />
+        </View>
+
+        <View style={{ marginTop: 30 }}>
+            <RoundedButton text='LOGIN' onPress={ () => ToastAndroid.show('Olá', ToastAndroid.SHORT) } />
+        </View>
+
+        <View style={ styles.formRegister }>
+            <Text>Não tem conta?</Text>
+            <Text style={ styles.formRegisterText }>Registrar-se</Text>
+        </View>
+
+        </View>
+    </View>
+    );
+}
+    
+    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    imageBackground: {
+        width: '100%',
+        height: '100%',
+        opacity: 0.7,
+        bottom: '30%'
+    }, 
+    form: {
+        width: '100%',
+        height: '40%',
+        backgroundColor: 'white',
+        position: 'absolute',
+        bottom: 0,
+        borderTopRightRadius: 40,
+        borderTopLeftRadius: 40,
+        padding: 30
+    },
+    formText: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    formIcon: {
+        width: 25,
+        height: 25,
+        marginTop: 5
+    },
+    formInput: {
+        flexDirection: 'row',
+        marginTop: 30,
+    },
+    formTextInput: {
+        flex: 1,
+        borderBottomWidth: 1,
+        borderBottomColor: '#AAAAAA',
+        marginLeft: 15
+    },
+    formRegister: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 30,
+    },
+    formRegisterText: {
+        fontStyle: 'italic',
+        color: 'orange',
+        borderBottomWidth: 1,
+        borderBottomColor: 'orange',
+        fontWeight: 'bold',
+        marginLeft: 10
+    },
+    logoContainer: {
+        position: 'absolute',
+        alignSelf: 'center',
+        top: '15%'
+    },
+    logoImage: {
+        width: 100,
+        height: 100
+    },
+    logoText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 20,
+        marginTop: 10,
+        fontWeight: 'bold'
+    },
+    
+});
