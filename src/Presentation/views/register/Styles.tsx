8f1ad416993524c1b20b7854d21ff84004f8af2c
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-02 21:19:05 -0300

    Separando arquivos de estilos

---
+++ src/Presentation/views/register/Styles.tsx
@@ -0,0 +1,76 @@
+import { StyleSheet } from 'react-native';
+
+const RegisterStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    imageBackground: {
+        width: '100%',
+        height: '100%',
+        opacity: 0.7,
+        bottom: '30%'
+    }, 
+    form: {
+        width: '100%',
+        height: '70%',
+        backgroundColor: 'white',
+        position: 'absolute',
+        bottom: 0,
+        borderTopRightRadius: 40,
+        borderTopLeftRadius: 40,
+        padding: 30
+    },
+    formText: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    formIcon: {
+        width: 25,
+        height: 25,
+        marginTop: 5
+    },
+    formInput: {
+        flexDirection: 'row',
+        marginTop: 30,
+    },
+    formTextInput: {
+        flex: 1,
+        borderBottomWidth: 1,
+        borderBottomColor: '#AAAAAA',
+        marginLeft: 15
+    },
+    formRegister: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 30,
+    },
+    formRegisterText: {
+        fontStyle: 'italic',
+        color: 'orange',
+        borderBottomWidth: 1,
+        borderBottomColor: 'orange',
+        fontWeight: 'bold',
+        marginLeft: 10
+    },
+    logoContainer: {
+        position: 'absolute',
+        alignSelf: 'center',
+        top: '5%',
+        alignItems: 'center'
+    },
+    logoImage: {
+        width: 100,
+        height: 100
+    },
+    logoText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 20,
+        marginTop: 10,
+        fontWeight: 'bold'
+    },
+    
+});
+
+export default RegisterStyles;
