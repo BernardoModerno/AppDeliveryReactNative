5fa6f5af106edf3118f2fdc49f6caf97deb10fb5
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-05 14:55:46 -0300

    JWT backend

--- src/Data/sources/remote/api/ApiDelivery.tsx
+++ src/Data/sources/remote/api/ApiDelivery.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ApiDelivery = axios.create({
-    baseURL: 'http://192.168.56.1:3000/api',
+    baseURL: 'http://172.28.16.1:3000/api',
     headers: {
         'Content-type': 'application/json'
     }
